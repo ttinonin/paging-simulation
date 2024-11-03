2bce0cf9cf10d6263974cd34dd8a2cfa6e6ceefc
ttinonin <tinonin935@gmail.com>
2024-11-03 15:25:25 -0300

    Adding LRU

--- main.py
+++ main.py
@@ -57,6 +57,69 @@ def fifo(sequence, pages, n_pages):
     print(f"Hit rate: ({hit_rate}/{len(sequence)})")
     print(f"Miss rate: ({miss_rate}/{len(sequence)})")
 
+def lru_remove_lowest(recent):
+    lowest = float('inf')
+    lowest_key = -1
+    lowest_page_index = -1
+
+    for i in recent:
+        if recent[i]["sequence_index"] < lowest:
+            lowest = recent[i]["sequence_index"]
+            lowest_key = i
+            lowest_page_index = recent[i]["page_index"]
+
+    recent.pop(lowest_key)
+
+    return lowest_page_index
+
+def lru(sequence, pages, n_pages):
+    recent = {}
+
+    hit_rate = 0
+    miss_rate = 0
+    is_full = False
+    page_index = 0
+    hit_location = -1
+
+    for sequence_index, number in enumerate(sequence):
+        is_hit = False
+
+        for page_pos, page in enumerate(pages):
+            if sequence_index == n_pages:
+                is_full = True
+
+            if number == page:
+                hit_rate += 1
+                is_hit = True
+                page_index = page_pos
+                break
+        
+        if is_full and not is_hit:
+            page_index = lru_remove_lowest(recent)
+
+        recent[number] = {
+            "sequence_index": sequence_index,
+            "page_index": page_index
+        }
+
+        pages[page_index] = number
+
+        print(f"page: {number}")
+        print_mem(pages, page_index, is_hit)
+        print()
+
+        if not is_full:
+            if page_index == n_pages - 1:
+                page_index = 0
+            else:
+                page_index += 1
+
+        if not is_hit:
+            miss_rate += 1
+
+    print(f"Hit rate: ({hit_rate}/{len(sequence)})")
+    print(f"Miss rate: ({miss_rate}/{len(sequence)})")
+
 def main():
     if len(sys.argv) != 3:
         print("Error: Incorrect number of argument values.")
